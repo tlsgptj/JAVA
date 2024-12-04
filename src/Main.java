4d9fd907b5415d2933e2f4ccdef2c6c214093100
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-04 16:43:15 +0900

    별자바수정

--- src/Main.java
+++ src/Main.java
@@ -68,9 +68,12 @@ class Dan {
 
                 System.out.println(" ");
 
-                for(int i = 1; i < 5; i++){
-                    for(int j = 5; j>i; j--) {
-                        System.out.print("* ");
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j<i; j++) {
+                        System.out.print(" ");
+                    }
+                    for (int j = 0; j<4-i; j++) {
+                        System.out.print("*");
                     }
                     System.out.println(" ");
                 }
