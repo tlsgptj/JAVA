c6f958d8a191c89612c1157988cfd6cdda40ec2c
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-06 15:57:11 +0900

    과제 완료

--- src/Day4.java
+++ src/Day4.java
@@ -4,7 +4,7 @@ public class Day4 {
     String str3 = "가을";
     String str4 = "겨울";
     String[] stars; //(1) 선언
-    //String []strs; -> 배열 변수
+
     void m1() {
         stars = new String[4]; //생성(방의 크기가 중요함)
         stars[0] = str1; //초기화
@@ -15,13 +15,36 @@ public class Day4 {
         for(int i=0; i<stars.length; i++) {
             System.out.println(stars[i]);
         }
-        System.out.println(stars[0]); //out (저장소다)
-        System.out.println(stars[1]);
     }
+
+    void m2() {
+        int[] is; //선언 지역변수 메모리 관점에서는 참조형 데이터
+        is = new int[2]; //생성
+
+        for(int i=0; i<is.length; i++) {
+            System.out.println(is[i]);
+        }
+    }
+
+    void out() { //isss 모든 데이터를 출력하시오
+        int[] is = {10, 20};
+        int[][] iss = {is, {30, 40, 50, 60}, {700}};
+        int[][][] isss = {iss, {{8000, 9000}, {10000}}}; //3차원 데이터를 뽑아오는 것
+        for (int i = 0; i < isss.length; i++) { //3차원
+            for (int j = 0; j < isss[i].length; j++) { //2차원
+                for(int k = 0; k < isss[i][j].length; k++) { //1차원
+                    System.out.println("isss[" + i + "][" + j + "][" + k + "]: " + isss[i][j][k]);
+                }
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
         Day4 day4 = new Day4();
         day4.m1();
+        day4.m2();
+        day4.out();
     }
 }
 
---
+++ src/Lotto.java
@@ -0,0 +1,84 @@
+import java.io.*;
+import java.util.*;
+
+class Tip1 {
+    String fname = "C:\\demo\\untitled\\src\\list.txt"; // 파일 경로
+    FileReader fr;
+    BufferedReader br;
+    String[] names = new String[8];
+
+    Tip1() {
+        try {
+            fr = new FileReader(fname);
+            System.out.println("fr: " + fr);
+            br = new BufferedReader(fr); // UTF-8로 읽어와야함
+        } catch (FileNotFoundException fe) {
+            pln("File not found: " + fname);
+        }
+    }
+   //파일을 읽는 함수
+    void read() {
+        String line = null;
+        try {
+            int i = 0;
+            while ((line = br.readLine()) != null) {
+                line = line.trim();
+                if (!line.isEmpty()) {
+                    names[i] = line;
+                    i++;
+                }
+            }
+        } catch (IOException ie) {
+            pln("파일 읽기 성공 : " + ie.getMessage());
+        } finally {
+            try {
+                if (br != null) br.close();
+            } catch (IOException ie) {
+                pln("파일 에러남 : " + ie.getMessage());
+            }
+        }
+    }
+   // 이름과 할당된 번호를 보여주는 함수
+    void showNames() {
+        for (int i = 0; i < names.length; i++) {
+            pln("names[" + i + "]: " + names[i]);
+        }
+    }
+
+    void pln(String str) {
+        System.out.println(str);
+    }
+}
+// 랜덤 함수 임포트로 불러오기
+class Tip2 {
+    Random r = new Random();
+    Tip1 tip1;
+
+    Tip2(Tip1 tip1) {
+        this.tip1 = tip1;
+    }
+
+    // 총인원과 당첨자, 당첨자의 번호 출력
+
+    void showRan() {
+        int i = r.nextInt(tip1.names.length);
+        System.out.println("총인원 : " + tip1.names.length);
+        pln("당첨자 : " + tip1.names[i] + " 당첨자의 번호 : " + i);
+
+    }
+
+
+    void pln(String str) {
+        System.out.println(str);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Tip1 t1 = new Tip1();
+        t1.read();
+        t1.showNames();
+        Tip2 t2 = new Tip2(t1);
+        t2.showRan();
+    }
+}
