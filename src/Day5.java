64c9b11a13a217796a049fe38fa7d1049ac43582
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-09 11:48:09 +0900

    treeset

--- src/Day5.java
+++ src/Day5.java
@@ -1,11 +1,23 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Day5 {
 
+    Vector v = new Vector();
+    TreeSet ts = new TreeSet();
     ArrayList list = new ArrayList(); //사이즈가 없어도 저장소 생성 가능 : 가변 배열
     void in() {
         list.add(10); //가변배열
         list.add(20);
+        v.add("봄");
+        v.add("여름");
+        v.add("가을");
+        v.add("겨울");
+        ts.add("하나");
+        ts.add("둘");
+        ts.add("셋");
+        ts.add("넷");
+        System.out.println(v.size());
+        System.out.println(ts.size());
     }
     void out() {
         Object obj1 = list.get(0);
@@ -14,6 +26,17 @@ public class Day5 {
         Integer iobj2 = (Integer)obj2; //객체 형태라 강제 형변환이 필요함(Object -> Integer)
         int i = iobj1.intValue();
         int j = iobj2.intValue(); //언박싱(참조형 -> 기본형)
+        for (int k = 0; i<v.size(); i++) {
+            Object obj = v.get(i);
+            String str = (String)obj;
+            System.out.println(str);
+        }
+        Iterator iter = ts.iterator(); // ts객체를 같이 참조하는거임
+        while(iter.hasNext()){
+            Object obj = iter.next();
+            String str = (String)obj; // object -> String
+            System.out.println(str);
+        }
         System.out.println("obj1 : " + i + " obj2 : " + j);
     }
 
