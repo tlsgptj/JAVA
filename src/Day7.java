6a042f7f47a2dd2db14bdb64f6cb42939d74b20a
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-10 11:16:17 +0900

    JVM이 생성자를 만들어줌

--- src/Day7.java
+++ src/Day7.java
@@ -1,18 +1,17 @@
-import java.lang.Object;
-
-class Day7 extends Object{
+class Day7{
     String name = "홍길동";
     void walk() {
-        System.out.println("걷는다");
+        System.out.println("걷는다 " + name);
     }
-    //부모와 자식관계를 가지게 됨 -> 부모에 있는 생성자를 자식에게
-    Day7() {
-        super(); // new Object();
-    }
+    //다른생성자가 없을때 JVM이 만들어줌
+    //부모와 자식관계를 가지게 됨 -> 부모에 있는 생성자를 자식에게 줌
 }
 class SuperMan extends Day7 {
     //부모에 있는 것을 사용이 가능함
     int power = 100;
+    SuperMan() {
+        super(); //부모의 생성자를 의미
+    }
     void fly() {
         System.out.println("난다 " + power);
     }
