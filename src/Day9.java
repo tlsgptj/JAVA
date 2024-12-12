dd2f2b7fa774896badc94ad04e680bbc910f03e2
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-12 10:52:59 +0900

    보안성 있는 설계를 위해

--- src/Day9.java
+++ src/Day9.java
@@ -1,9 +1,27 @@
 public class Day9 {
-    public int i;
-    public Day9() {
-        System.out.println("A()");
-    }
-    public void m() {
+    public int publicField;
+    protected int protectedField;
+    int defaultField;
+    private int privateField;
+    public void publicMethod() {
         System.out.println("m()");
+    }
+    protected void protectedMethod() {
+        System.out.println("protectedMethod");
+    }
+
+    void defaultMethod() {
+        System.out.println("defaultMethod");
+    }
+
+    private void privateMethod() {
+        System.out.println("privateMethod");
+    }
+    public static void main(String[] args) {
+        Day9 day9 = new Day9();
+        day9.defaultField
+        int privateField1 = day9.privateField;
+        int protectedField1 = day9.protectedField;
+        int publicField1 = day9.publicField;
     }
 }
