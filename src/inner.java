764fc029f931129b8b8ae21df596f8f722bd38af
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-16 10:32:51 +0900

    아니 이너클래스!?

--- src/inner.java
+++ src/inner.java
@@ -7,11 +7,24 @@ class A7 {
             System.out.println("AInner m()");
         }
     }
-    public static class AInner2 {
+    static class AInner2 {
         int j;
         void m() {
             System.out.print("Ainner m()");
         }
+    }
+}
+class B7 {
+    interface BInner {
+        //상수가 오겠지?
+        int i = 10;
+        void m();
+    }
+}
+class BUser123 implements B7.BInner {
+    public void m(){};
+    public static void main(String args) {
+        System.out.println("BInnerInterChild m()");
     }
 }
 public class inner {
