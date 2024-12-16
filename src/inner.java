86267d6e90300a24d3ce008dca4bc43c503890d5
신혜서 <125953570+tlsgptj@users.noreply.github.com>
2024-12-16 11:35:30 +0900

    반딧불

--- src/inner.java
+++ src/inner.java
@@ -41,18 +41,37 @@ interface C8 {
         void m2();
     }
 }
+class D8 {
+    int i;
+    D8() {
+        System.out.println("D()");
+    }
+    void m() {
+        System.out.println("m()");
+    }
+    class DInter {
+        void use() {
+            System.out.println("i : " + i);
+            m(); //이것도 호출이 가능함
+        }
+    }
+}
 public class inner {
     public static void main(String[] args) {
         new A7().new AInner();
         A7.AInner in1 = new A7(). new AInner();
+        D8.DInter in = new D8().new DInter();
         System.out.println("in1.i" + in1.i);
         in1.m();
 
         A7.AInner2 in2 = new A7.AInner2(); //이너클래스 접근법은 좀 다름
         System.out.println("in1.i" + in2.j);
         in1.m();
+        in.use();
     }
 }
+
+
 
 
 
